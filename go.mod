aaf253b28e76de1406d2a9317b8f302d7d23fefe
10Narratives <vanyatertyshnikov@yandex.ru>
2024-12-15 20:02:14 +0300

    Add Element struct

---
+++ go.mod
@@ -0,0 +1,3 @@
+module chemical-tool
+
+go 1.23.4
---
+++ internal/models/element.go
@@ -0,0 +1,7 @@
+package models
+
+// Element represents a chemical element with its symbol and atomic weight.
+type Element struct {
+	Symbol       string  // The symbol of the element, e.g., "H" for hydrogen
+	AtomicWeight float64 // The atomic weight of the element, e.g., 1.008 for hydrogen
+}
