15aa56734227afb56eb2c00915519e6e4d9c7a32
10Narratives <vanyatertyshnikov@yandex.ru>
2024-12-15 21:58:03 +0300

    Add MolarMassService

--- internal/database/store.go
+++ internal/database/store.go
@@ -30,10 +30,10 @@ type Store struct {
 //	       If no element is found, the returned element will be empty
 //	       and the error will be nil.
 func (store Store) GetElement(symbol string) (models.Element, error) {
-	row := store.db.QueryRow("SELECT symbol atomic_weight FROM periodic_table WHERE symbol = ?", symbol)
+	row := store.db.QueryRow("SELECT name symbol atomic_weight FROM periodic_table WHERE symbol = ?", symbol)
 
 	gottenElement := models.Element{}
-	err := row.Scan(&gottenElement.Symbol, &gottenElement.AtomicWeight)
+	err := row.Scan(&gottenElement.Name, &gottenElement.Symbol, &gottenElement.AtomicWeight)
 	if errors.Is(err, sql.ErrNoRows) {
 		return models.Element{}, sql.ErrNoRows
 	}
@@ -42,4 +42,27 @@ func (store Store) GetElement(symbol string) (models.Element, error) {
 	}
 
 	return gottenElement, nil
+}
+
+// GetElements retrieves the elements that make up a given compound.
+//
+// It takes a 'target' of type models.Compound, which contains the
+// data representing the compound's elements. The function iterates
+// over the symbols in the target's data, fetching each corresponding
+// element from the store. If any retrieval fails, it returns an error.
+//
+// Returns:
+//   - A slice of models.Element containing the elements of the compound,
+//     or nil if an error occurred.
+//   - An error indicating what went wrong, if applicable.
+func (store Store) GetElements(target models.Compound) ([]models.Element, error) {
+	var gottenElements []models.Element = make([]models.Element, 0)
+	for symbol := range target.Data {
+		element, err := store.GetElement(symbol)
+		if err != nil {
+			return nil, err
+		}
+		gottenElements = append(gottenElements, element)
+	}
+	return gottenElements, nil
 }
